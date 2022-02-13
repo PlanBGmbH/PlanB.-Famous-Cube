a5d29a71fbd1ccb6c61b9e046bed78c72b7fe64b
Felix Rohmeier <felix.rohmeier@plan-b-gmbh.com>
2022-02-13 15:56:11 +0100

    Improvments Record Button

--- Nreal/Assets/NrealExtensions/Prefabs/Scripts/RecordButton/RecordController.cs
+++ Nreal/Assets/NrealExtensions/Prefabs/Scripts/RecordButton/RecordController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Android;
 using UnityEngine.UI;
 
 /// <summary>
@@ -23,6 +24,41 @@ public class RecordController : MonoBehaviour
     /// </summary>
     private bool recordingInProgress = false;
 
+    /// <summary>
+    /// Quality of recording
+    /// </summary>
+    public enum ResolutionLevel
+    {
+        High,
+        Middle,
+        Low,
+    }
+
+    public static void RequestPermission()
+    {
+        #if PLATFORM_ANDROID
+            if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageRead))
+            {
+                Permission.RequestUserPermission(Permission.ExternalStorageRead);
+            }
+
+            if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageWrite))
+            {
+                Permission.RequestUserPermission(Permission.ExternalStorageWrite);
+            }
+
+            if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+            {
+                Permission.RequestUserPermission(Permission.Microphone);
+            }
+
+            if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+            {
+                Permission.RequestUserPermission(Permission.Camera);
+            }
+    #endif
+    }
+
     /// <summary> Save the video to Application.persistentDataPath. </summary>
     /// <value> The full pathname of the video save file. </value>
     public string VideoSavePath
@@ -30,10 +66,8 @@ public class RecordController : MonoBehaviour
         get
         {
             string timeStamp = Time.time.ToString().Replace(".", "").Replace(":", "");
-            string filename = string.Format("RC_Record_{0}.mp4", timeStamp);
-            string filepath = Path.Combine(Application.persistentDataPath, filename);
-            //filepath = filepath.Replace("/", @"\");
-            return filepath;
+            string filename = string.Format("Nreal_Record_{0}.mp4", timeStamp);
+            return Path.Combine(Application.persistentDataPath, filename);
         }
     }
 
@@ -43,17 +77,18 @@ public class RecordController : MonoBehaviour
     /// <summary> Starts this object. </summary>
     void Start()
     {
-        CreateVideoCaptureTest();
+        RecordController.RequestPermission();
     }
 
-    /// <summary> Tests create video capture. </summary>
-    void CreateVideoCaptureTest()
+    void CreateVideoCapture(Action callback)
     {
         NRVideoCapture.CreateAsync(false, delegate (NRVideoCapture videoCapture)
         {
+            NRDebugger.Info("Created VideoCapture Instance!");
             if (videoCapture != null)
             {
                 m_VideoCapture = videoCapture;
+                callback?.Invoke();
             }
             else
             {
@@ -67,6 +102,14 @@ public class RecordController : MonoBehaviour
     /// </summary>
     public void ToggleRecording()
     {
+        if (m_VideoCapture == null)
+        {
+            CreateVideoCapture(() =>
+            {
+                StartVideoCapture();
+            });
+        }
+
         if (!recordingInProgress)
         {
             recordingInProgress = true;
@@ -82,36 +125,57 @@ public class RecordController : MonoBehaviour
     /// <summary> Starts video capture. </summary>
     public void StartVideoCapture()
     {
-        Resolution cameraResolution = NRVideoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
-        NRDebugger.Info(cameraResolution);
-
-        int cameraFramerate = NRVideoCapture.GetSupportedFrameRatesForResolution(cameraResolution).OrderByDescending((fps) => fps).First();
-        NRDebugger.Info(cameraFramerate);
-
-        if (m_VideoCapture != null)
+        if (m_VideoCapture == null || m_VideoCapture.IsRecording)
         {
-            NRDebugger.Info("Created VideoCapture Instance!");
-            CameraParameters cameraParameters = new CameraParameters();
-            cameraParameters.hologramOpacity = 0.0f;
-            cameraParameters.frameRate = cameraFramerate;
-            cameraParameters.cameraResolutionWidth = cameraResolution.width;
-            cameraParameters.cameraResolutionHeight = cameraResolution.height;
-            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
-            cameraParameters.blendMode = BlendMode.Blend;
-
-            m_VideoCapture.StartVideoModeAsync(cameraParameters, OnStartedVideoCaptureMode);
-
-            RecordingIndicator.color = Color.red;
+            NRDebugger.Warning("Can not start video capture!");
+            return;
         }
+
+        CameraParameters cameraParameters = new CameraParameters();
+        Resolution cameraResolution = GetResolutionByLevel(ResolutionLevel.High);
+        cameraParameters.hologramOpacity = 0.0f;
+        cameraParameters.frameRate = cameraResolution.refreshRate;
+        cameraParameters.cameraResolutionWidth = cameraResolution.width;
+        cameraParameters.cameraResolutionHeight = cameraResolution.height;
+        cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+        cameraParameters.blendMode = BlendMode.Blend;
+        cameraParameters.audioState = NRVideoCapture.AudioState.MicAudio;
+
+        m_VideoCapture.StartVideoModeAsync(cameraParameters, OnStartedVideoCaptureMode, true);
+
+        RecordingIndicator.color = Color.red;
+    }
+
+    private Resolution GetResolutionByLevel(ResolutionLevel level)
+    {
+        var resolutions = NRVideoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height);
+        Resolution resolution = new Resolution();
+        switch (level)
+        {
+            case ResolutionLevel.High:
+                resolution = resolutions.ElementAt(0);
+                break;
+            case ResolutionLevel.Middle:
+                resolution = resolutions.ElementAt(1);
+                break;
+            case ResolutionLevel.Low:
+                resolution = resolutions.ElementAt(2);
+                break;
+            default:
+                break;
+        }
+        return resolution;
     }
 
     /// <summary> Stops video capture. </summary>
     public void StopVideoCapture()
     {
-        if (m_VideoCapture == null)
+        if (m_VideoCapture == null || !m_VideoCapture.IsRecording)
         {
+            NRDebugger.Warning("Can not stop video capture!");
             return;
         }
+
         NRDebugger.Info("Stop Video Capture!");
         m_VideoCapture.StopRecordingAsync(OnStoppedRecordingVideo);
     }
@@ -120,6 +184,12 @@ public class RecordController : MonoBehaviour
     /// <param name="result"> The result.</param>
     void OnStartedVideoCaptureMode(NRVideoCapture.VideoCaptureResult result)
     {
+        if (!result.success)
+        {
+            NRDebugger.Info("Started Video Capture Mode faild!");
+            return;
+        }
+
         NRDebugger.Info("Started Video Capture Mode!");
         m_VideoCapture.StartRecordingAsync(VideoSavePath, OnStartedRecordingVideo);
     }
@@ -128,6 +198,12 @@ public class RecordController : MonoBehaviour
     /// <param name="result"> The result.</param>
     void OnStartedRecordingVideo(NRVideoCapture.VideoCaptureResult result)
     {
+        if (!result.success)
+        {
+            NRDebugger.Info("Started Recording Video Faild!");
+            return;
+        }
+
         NRDebugger.Info("Started Recording Video!");
     }
 
@@ -135,6 +211,12 @@ public class RecordController : MonoBehaviour
     /// <param name="result"> The result.</param>
     void OnStoppedRecordingVideo(NRVideoCapture.VideoCaptureResult result)
     {
+        if (!result.success)
+        {
+            NRDebugger.Info("Stopped Recording Video Faild!");
+            return;
+        }
+
         NRDebugger.Info("Stopped Recording Video!");
         m_VideoCapture.StopVideoModeAsync(OnStoppedVideoCaptureMode);
     }
@@ -143,8 +225,18 @@ public class RecordController : MonoBehaviour
     /// <param name="result"> The result.</param>
     void OnStoppedVideoCaptureMode(NRVideoCapture.VideoCaptureResult result)
     {
-        m_VideoCapture.Dispose();
-        RecordingIndicator.color = new Color(1f, 1f, 1f, 0.3921569f);
         NRDebugger.Info("Stopped Video Capture Mode!");
+
+        // Release video capture resource.
+        m_VideoCapture.Dispose();
+        m_VideoCapture = null;
+        RecordingIndicator.color = new Color(1f, 1f, 1f, 0.3921569f);
+    }
+
+    void OnDestroy()
+    {
+        // Release video capture resource.
+        m_VideoCapture?.Dispose();
+        m_VideoCapture = null;
     }
 }
